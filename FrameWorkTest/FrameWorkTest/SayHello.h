472dec1c16c60f0ccaf30afa041ed54998ce27a5
Eternal_Jovi <eternal.l.jovi@gmail.com>
2018-01-05 03:43:15 +0800

    make framework manual

---
+++ FrameWorkTest/FrameWorkTest/SayHello.h
@@ -0,0 +1,15 @@
+//
+//  SayHello.h
+//  FrameWorkTest
+//
+//  Created by L Jovi on 05/01/2018.
+//  Copyright © 2018 L Jovi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SayHello : NSObject
+
+- (void)sayHello;
+
+@end
---
+++ FrameWorkTest/FrameWorkTest/SayHello.m
@@ -0,0 +1,18 @@
+//
+//  SayHello.m
+//  FrameWorkTest
+//
+//  Created by L Jovi on 05/01/2018.
+//  Copyright © 2018 L Jovi. All rights reserved.
+//
+
+#import "SayHello.h"
+
+@implementation SayHello
+
+-(void)sayHello
+{
+    NSLog(@"你好，第一次见面，请多关照");
+}
+
+@end
