e48e045bb2bacbc91e533969de6ba78179184ca8
Eternal_Jovi <eternal.l.jovi@gmail.com>
2018-04-02 00:21:07 +0800

    Add Testing exercise.

---
+++ Testing/BullsEye/BullsEyeTests/BullsEyeTests.swift
@@ -0,0 +1,44 @@
+//
+//  BullsEyeTests.swift
+//  BullsEyeTests
+//
+//  Created by L Jovi on 2018/4/1.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+@testable import BullsEye
+
+class BullsEyeTests: XCTestCase {
+    var gameUnderTest: BullsEyeGame!
+    
+    override func setUp() {
+        super.setUp()
+        gameUnderTest = BullsEyeGame()
+        gameUnderTest.startNewGame()
+    }
+    
+    override func tearDown() {
+        gameUnderTest = nil
+        super.tearDown()
+    }
+    
+    func testScoreIsComputed() {
+        // 1. given
+        let guess = gameUnderTest.targetValue + 5
+        
+        // 2. when
+        _ = gameUnderTest.check(guess: guess)
+        
+        // 3. then
+        XCTAssertEqual(gameUnderTest.scoreRound, 95, "Score computed from guess is wrong")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
