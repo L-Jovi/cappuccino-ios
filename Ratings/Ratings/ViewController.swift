6c472a655ebcc470cc0c8c08adc240c7755d9f96
Eternal_Jovi <eternal.l.jovi@gmail.com>
2018-04-27 09:45:07 +0800

    Add bar item image of Rating simple app.

---
+++ Ratings/Ratings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by L Jovi on 2018/4/26.
+//  Copyright © 2018 L Jovi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
