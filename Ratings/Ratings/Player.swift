65201bc80037c81bada8dacac7a221e70d0ec3dc
Eternal_Jovi <eternal.l.jovi@gmail.com>
2018-04-28 00:03:35 +0800

    Finish storyboard part 1 which make simple Rating app.

---
+++ Ratings/Ratings/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by L Jovi on 2018/4/27.
+//  Copyright © 2018 L Jovi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    // MARK: - Properties
+    var name: String?
+    var game: String?
+    var rating: Int
+}
