9221695a3fbb13fe3ed4bfdfa8f0b448c0b30445
Eternal_Jovi <eternal.l.jovi@gmail.com>
2018-05-27 15:24:34 +0800

    Add animation login design.

---
+++ AnimationLogin/AnimationLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationLogin
+//
+//  Created by L Jovi on 2018/5/27.
+//  Copyright © 2018 L Jovi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
