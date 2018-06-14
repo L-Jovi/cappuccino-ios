0872fec6022fe0ea121994d5b2aedc2e1a3fdc38
Eternal_Jovi <eternal.l.jovi@gmail.com>
2018-06-14 17:20:15 +0800

    Support haul HMR.

--- ReactNativePractice/index.js
+++ ReactNativePractice/index.js
@@ -1,3 +1,19 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('ReactNativePractice', () => App);
+import 'haul/hot/patch'
+import {
+  makeHot,
+  tryUpdateSelf,
+  callOnce,
+  clearCacheFor,
+  redraw,
+} from 'haul/hot'
+import { AppRegistry } from 'react-native'
+import App from './App'
+
+AppRegistry.registerComponent('ReactNativePractice', makeHot(() => App))
+
+if (module.hot) {
+  module.hot.accept(['./App'], () => {
+    clearCacheFor(require.resolve('./App'));
+    redraw(() => require('./App').default);
+  })
+}
